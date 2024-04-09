668c309a2b777f7e09756fa26c56ecb88218b6ee
simpwae <naqashamjad777@gmail.com>
2024-04-10 03:29:24 +0500

    '...'

---
+++ script.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".auto_type", {
+  strings: [
+    "Monthly Family Support,",
+    "Daily Meals,",
+    "IT and Vocalation training",
+    "Healthcare and lots More.",
+  ],
+  typeSpeed: 60,
+  loop: true,
+});
